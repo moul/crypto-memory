25ae9a8e4ebd8824a7e3c5e047d0d575a00d68f5
Manfred Touron <m@42.am>
2019-05-16 11:57:13 +0200

    feat: generate pem from ssh pub keys

--- gen.sh
+++ gen.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+rm -rf out
 mkdir -p out
+
+# openssh rsa 4096
 ssh-keygen -t rsa -b 4096 -C "" -f ./out/A -q -N ""
-ssh-keygen -t ed25519 -C "" -f ./out/B -q -N ""
-ssh-keygen -t rsa -b 1024 -C "" -f ./out/C -q -N ""
+ssh-keygen -f out/A.pub -e -m pem > out/A.pem
+
+# openssh rsa 1024
+ssh-keygen -t rsa -b 1024 -C "" -f ./out/B -q -N ""
+ssh-keygen -f out/B.pub -e -m pem > out/B.pem
+
+# openssh ed25519
+ssh-keygen -t ed25519 -C "" -f ./out/C -q -N ""
