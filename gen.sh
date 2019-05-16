ea43ba438af9a0631bcb72904aa3ee2c7fa77cb7
Manfred Touron <m@42.am>
2019-05-16 14:33:25 +0200

    feat: shuf results

--- gen.sh
+++ gen.sh
@@ -1,17 +1,27 @@
-#!/bin/sh
+#!/bin/sh -e
 
-rm -rf out
-mkdir -p out
+rm -rf out shuf
+mkdir -p out shuf
 
-# openssh rsa 4096
-ssh-keygen -t rsa -b 4096 -C "" -f ./out/A -q -N ""
-ssh-keygen -f out/A.pub -e -m pem > out/A.pem
-openssl rsa -RSAPublicKey_in -in out/A.pem -inform PEM -outform DER -out out/A.der -RSAPublicKey_out
+(set -x
 
-# openssh rsa 1024
-ssh-keygen -t rsa -b 1024 -C "" -f ./out/B -q -N ""
-ssh-keygen -f out/B.pub -e -m pem > out/B.pem
-openssl rsa -RSAPublicKey_in -in out/B.pem -inform PEM -outform DER -out out/B.der -RSAPublicKey_out
+    # openssh rsa 4096
+    ssh-keygen -t rsa -b 4096 -C "" -f ./out/A -q -N ""
+    ssh-keygen -f out/A.pub -e -m pem > out/A.pem
+    openssl rsa -RSAPublicKey_in -in out/A.pem -inform PEM -outform DER -out out/A.der -RSAPublicKey_out 2>/dev/null
 
-# openssh ed25519
-ssh-keygen -t ed25519 -C "" -f ./out/C -q -N ""
+    # openssh rsa 1024
+    ssh-keygen -t rsa -b 1024 -C "" -f ./out/B -q -N ""
+    ssh-keygen -f out/B.pub -e -m pem > out/B.pem
+    openssl rsa -RSAPublicKey_in -in out/B.pem -inform PEM -outform DER -out out/B.der -RSAPublicKey_out 2>/dev/null
+
+    # openssh ed25519
+    ssh-keygen -t ed25519 -C "" -f ./out/C -q -N ""
+)
+
+# shuffle
+counter=0
+for file in $(ls out/*); do
+    counter=$((counter+1))
+    cp $file shuf/$counter
+done
